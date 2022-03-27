a527d5ad511d24e2518417015ee71905828f1302
Smael Nicolas <nico.smael@gmail.com>
2022-03-27 15:33:38 -0300

    add flags

--- src/Pages/Home/MainContent/Groups/GroupsMatchs/MatchFormat/MatchFormat.jsx
+++ src/Pages/Home/MainContent/Groups/GroupsMatchs/MatchFormat/MatchFormat.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Score, Team } from "../../../../../../CommonStyledComponents";
 
-function MatchFormat({ score, info, team1, team2 }) {
+function MatchFormat({ score, info, team1, team2, flag1, flag2 }) {
+	const [flag1String] = useState(`https://flagcdn.com/${flag1}.svg`);
+	const [flag2String] = useState(`https://flagcdn.com/${flag2}.svg`);
+
 	return (
 		<FormatContainer>
-			<Team1>{team1 || "TBD"}</Team1>
+			<Team1>
+				{team1 || "TBD"} <ImgTeam src={flag1String} />
+			</Team1>
 			<Score1>{score[0] || "TBD"}</Score1>
 			<Separator> - </Separator>
 			<Score2>{score[1] || "TBD"}</Score2>
-			<Team2>{team2 || "TBD"}</Team2>
+			<Team2>
+				<ImgTeam src={flag2String} />
+				{team2 || "TBD"}
+			</Team2>
 			<Info>{info || "TBD"}</Info>
 		</FormatContainer>
 	);
@@ -30,9 +38,15 @@ const FormatContainer = styled.div`
 
 const Team1 = styled(Team)`
 	grid-area: team1;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
 `;
 const Team2 = styled(Team)`
 	grid-area: team2;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
 `;
 const Score1 = styled(Score)`
 	grid-area: score1;
@@ -50,4 +64,8 @@ const Info = styled.div`
 const Separator = styled.div`
 	grid-area: separator;
 	color: var(--clrs--blue);
+`;
+
+const ImgTeam = styled.img`
+	width: 25px;
 `;
